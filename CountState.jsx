9430dc1b571a4c620a6f1ff1482133995c58d7e9
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-17 11:14:39 +0530

    Update CountState.jsx

--- CountState.jsx
+++ CountState.jsx
@@ -1,1 +1,25 @@
+import {useState} from 'react';
+export default function CountState(){
+
+let[count,setCount] = useState(0);
+
+   function increaseCount(){
+    setCount(count+1);
+    
+  }
+ return (
+   <div>
+   <h1>Count:{count}</h1>
+  <button onClick={increaseCount}>Increase</button>
+     
+   </div>
+ );
+  
+}
+
+
+
+
+
+
 
