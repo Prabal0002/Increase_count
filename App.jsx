4528cc1a840436f991990a2fa0ad52240a5a1806
Prabal Singh Bhadoriya <129841865+Prabal0002@users.noreply.github.com>
2024-07-17 11:19:00 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import './App.css';
+import CountState from './CountState.jsx';
+
+
+function App(){
+  
+    return (
+      <div className="App">
+        <h1>States in react</h1>
+    
+       <CountState />
+          
+      </div>
+    );
+  }
+
+
+export default App;
 
